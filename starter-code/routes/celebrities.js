ea1fb1ce513b06e6c5c20fb0f4cf3f15f1df4b3d
Ann Lampa <riann08@yahoo.com>
2020-12-28 23:49:30 +0100

    Update and delete for celebrities (iteration 4 and 5)

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,47 +1,83 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 const CelebrityModel = require("./../model/Celebrity");
 
-router.get("/", async(req, res, next) => {
-  try{
+router.get("/", async (req, res, next) => {
+  try {
     const allCelebrities = await CelebrityModel.find();
 
-    res.render("celebrities/index", {allCelebrities});
+    res.render("celebrities/index", { allCelebrities });
   } catch (err) {
-    next (err);
+    next(err);
   }
-  });
+});
 
-  
-  router.get("/new",(req, res, next) => {
-    try{
-      res.render("celebrities/new");
-    } catch (err) {
-      next (err);
-    }
-  });
+router.get("/new", (req, res, next) => {
+  try {
+    res.render("celebrities/new");
+  } catch (err) {
+    next(err);
+  }
+});
 
-  router.post("/", async(req, res, next) => {
-    const newCelebrity = { ... req.body};
-    console.log(newCelebrity);
+router.post("/", async (req, res, next) => {
+  const newCelebrity = { ...req.body };
+  console.log(newCelebrity);
 
-    try{
-      await CelebrityModel.create(newCelebrity);
-      res.redirect("/celebrities/");
-    } catch (err) {
-      next (err);
-      res.redirect("/celebrities/new"); 
-    }
-  });
-  
-  router.get("/:id", async(req, res, next) => {
-    try{
-      const celebrity = await CelebrityModel.findById(req.params.id);
-      res.render("celebrities/show", celebrity);
-    } catch (err) {
-      next (err);
-    }
-    });
+  try {
+    await CelebrityModel.create(newCelebrity);
+    res.redirect("/celebrities/");
+  } catch (err) {
+    next(err);
+    res.redirect("/celebrities/new");
+  }
+});
+
+router.post("/:id/delete", async (req, res, next) => {
+  try {
+    const celebrity = await CelebrityModel.findByIdAndRemove(req.params.id);
+    res.render("celebrities/index", celebrity);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id/delete", async (req, res, next) => {
+  try {
+    await CelebrityModel.findByIdAndDelete(req.params.id);
+    res.redirect("/celebrities/");
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const celebrity = await CelebrityModel.findById(req.params.id);
+    res.render("celebrities/show", celebrity);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id/edit", async (req, res, next) => {
+  try {
+    const celebrity = await CelebrityModel.findById(req.params.id);
+    res.render("celebrities/edit", celebrity);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/:id", async (req, res, next) => {
+  try {
+    const updatedCeleb = {...req.body};
+    await CelebrityModel.findByIdAndUpdate(req.params.id, updatedCeleb);
+    res.redirect("/celebrities/");
+  } catch (err) {
+    next(err);
+  }
+});
 
 
-  module.exports = router;
+module.exports = router;
